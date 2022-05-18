f87ca344db309b6249390e9606434c7a24de7f71
connor.lol <93891077+xqrcd@users.noreply.github.com>
2022-05-18 05:50:31 -0400

    wording

--- example.env.sh
+++ example.env.sh
@@ -1,7 +1,6 @@
 # Copy this file to .env.sh and fill in the environment variables below. The
-# .env.sh file is only used when running the app locally; see the deno.json file
-# for more info. In Deno Deploy, these environment variables need to be set
-# using Deploy's web interface
+# .env.sh file is only used when running the app locally. In Deno Deploy, these
+# environment variables need to be set using Deploy's web interface
 
 export DEV=1 # Comment this out in production
 export SUPABASE_URL= # supabase start -> API URL
