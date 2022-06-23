0a08383c0a4cd8ec9a3bbb94337a0d0205079a42
Connor Logan <93891077+connorlogin@users.noreply.github.com>
2022-06-23 16:32:16 -0400

    charset stuff still

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 // Copyright 2022 Connor Logan. All rights reserved. MIT License.
 
-export * from "https://deno.land/x/cav@0.2.0-alpha.3/mod.ts";
+export * from "https://deno.land/x/cav@0.2.0-alpha.4/mod.ts";
