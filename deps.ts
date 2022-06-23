ec4f88487ec8cb5fe3f50cda2c4b79f1c855ae61
Connor Logan <93891077+connorlogin@users.noreply.github.com>
2022-06-23 14:57:25 -0400

    cav@0.2.0-alpha.2

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 // Copyright 2022 Connor Logan. All rights reserved. MIT License.
 
-export * from "https://deno.land/x/cav@0.2.0-alpha.1/mod.ts";
+export * from "https://deno.land/x/cav@0.2.0-alpha.2/mod.ts";
