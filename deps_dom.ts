200fb5aae65f1348b0dcbf9bb656f34f1d492809
Connor Logan <93891077+connorlogin@users.noreply.github.com>
2022-06-23 15:17:44 -0400

    charset bugs

--- deps_dom.ts
+++ deps_dom.ts
@@ -1,4 +1,4 @@
 // Copyright 2022 Connor Logan. All rights reserved. MIT License.
 // This module is browser-only.
 
-export * from "https://deno.land/x/cav@0.2.0-alpha.2/dom.ts";
+export * from "https://deno.land/x/cav@0.2.0-alpha.3/dom.ts";
